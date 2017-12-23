3adb4db39e11264b8d04f280484487728530d6e9
aminul9 <aminultpi34@gmail.com>
2017-12-23 07:41:41 +0600

    Update 1292_trouble_with_pentagone.cpp

--- 1292_trouble_with_pentagone.cpp
+++ 1292_trouble_with_pentagone.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double input;
+    while(cin>>input){
+
+    cout<<fixed<<setprecision(10)<< input*sin(108*acos(-1)/180)/sin(63*acos(-1)/180)<<endl;
+    }
+
+    return 0;
+}
